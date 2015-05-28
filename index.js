40f14062e740e3758e54c7ce6848d14be7f6568e
thefourtheye <thechargingvolcano@gmail.com>
2015-05-28 23:41:53 +0530

    Including validations

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var path = require("path"),
     glob = require("glob");
 
-function requirer(pattern) {
+function requirer(pattern, options) {
     var modules = {},
-        files = glob.sync(pattern);
+        files;
+
+    if (typeof pattern !== "string" || !(pattern = pattern.trim())) {
+        throw new Error("Pattern must be a valid String");
+    }
+
+    if (arguments.length === 2)
+
+    if (Object.prototype.toString.call(options) === "[object Object]") {
+        throw new Error("Options must be a valid Object");
+    }
+
+    files = glob.sync(pattern, options);
 
     files.forEach(function(currentFile) {
         var fileName = path.basename(currentFile);
